459ebb8fa5e9453dab29b5fd42b8c6a9bfcf10ee
Taylor Thomas <taylor@oftaylor.com>
2024-12-02 13:28:57 -0500

    Adds additional work configuration

--- work/home.nix
+++ work/home.nix
@@ -3,8 +3,21 @@
     home.username = "taylor";
     home.homeDirectory = "/Users/${home.username}";
 
-    home.packages = [ pkgs.kubectl pkgs.kind ];
+    home.packages = [ pkgs.kubectl pkgs.kind pkgs.redis pkgs.oras ];
 
     programs.git = { userEmail = "taylor@cosmonic.com"; };
+
+    programs.vscode = {
+      extensions = with pkgs.vscode-extensions; [
+        pkgs.vscode-marketplace.adpyke.codesnap
+        pkgs.vscode-marketplace.dracula-theme.theme-dracula
+        github.vscode-github-actions
+        pkgs.vscode-marketplace-release.eamodio.gitlens
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        pkgs.vscode-marketplace.bytecodealliance.wit-idl
+        pkgs.vscode-marketplace.redhat.vscode-yaml
+        ms-vscode-remote.remote-ssh
+      ];
+    };
   };
 }
