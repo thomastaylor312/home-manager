c2f3275dab7ea0303ff0acc1a6a872101c0058cb
Taylor Thomas <taylor@oftaylor.com>
2025-01-21 22:20:14 -0700

    Add additional trusted cache for nix community

--- home.nix
+++ home.nix
@@ -40,11 +40,13 @@
     substituters = [
       "https://cache.nixos.org"
       "https://crane.cachix.org"
+      "https://nix-community.cachix.org"
       "https://imperial-archives.dojo-nominal.ts.net/oftaylor"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "oftaylor:/F+43JMUT9r7G5lKdvvIDoF+KBNdGR6ZWevakY0BjZo="
     ];
   };
