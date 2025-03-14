5b0030fc16b4a442b92daf97105fd6e053ca3ae5
Taylor Thomas <taylor@oftaylor.com>
2025-03-14 12:32:26 -0600

    Pin wasm-tools

--- work/pkgs/default.nix
+++ work/pkgs/default.nix
@@ -1,1 +1,5 @@
-pkgs: { oci-cli = pkgs.callPackage ./oci-cli { }; }
+pkgs: {
+  oci-cli = pkgs.callPackage ./oci-cli { };
+  # We temporarily need to pin because later versions break Go
+  wasm-tools = pkgs.callPackage ./wasm-tools { };
+}
